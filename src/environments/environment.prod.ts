f8be27e326eebee97963e6d532d8f838750e4b65
codeachirajib <rajib.gupta.web@gmail.com>
2021-09-16 23:33:35 +0530

    Update env production file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+
+const baseUrl = 'http://localhost:3000';
+const base=`http://localhost:3000`;
 export const environment = {
-  production: true
+  production: true,
+  urlAddress: baseUrl,
+   baseImageUrl:base,
+  endPoints: {
+    // auth: { signIn: `${baseUrl}/signIn` },
+    auth:{master:`${baseUrl}/masterlogin`},
+    Supervisor:{suplogin:`${baseUrl}/suplogin`},
+    employee: {
+      createEmployee: `${baseUrl}/path/to/create`,
+      loginEmoployee: `${baseUrl}/login`,
+    },
+  },
 };
